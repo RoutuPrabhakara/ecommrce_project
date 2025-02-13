fb1b15a79219a2798fb7e8b5adda924e95f17b3c
RoutuPrabhakara <routuprabhakar2000@gmail.com>
2025-02-13 16:14:49 +0530

    updates

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,22 +1,49 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 function Signup() {
+  const[formdata,SetFormData]=useState({
+    name:'',
+    email:'',
+    password:''
+})
+
+const Handler =(e)=>{
+  SetFormData({...formdata,[e.target.name]:e.target.value})
+}
+
+const submitHandler = async(e)=>{
+  e.preventDefault();
+  try {
+   
+    await axios.post('http://localhost:5000/SignUp',formdata)
+    .then((response)=>{
+      console.log('Data is connected',response.data)
+      alert('Registration Successfull')
+      SetFormData({ name: '', email: '', password: '' });
+    })
+  } catch (error) {
+    console.log(error)
+    alert('Error registering. Please try again.');
+  }
+}
   return (
     <div>
           <div className="container-fluid p3 p-md-5 bg-light mt-4 mb-4">
             <div className="row">
-                <div className="col-md-5 m-auto">
+              <form action="" onSubmit={submitHandler}>
+              <div className="col-md-5 m-auto">
                     <div className="card p-3 shadow-sm">
                         <h1 className="fs-3 text-center fw-blod">Create Account</h1>
 
                         <label htmlFor="email">Username <span className='text-danger'>*</span></label>
-                        <input type="text" name="" id="email" className='form-control mb-2' placeholder='Enter  your Username'  required />
+                        <input type="text" name="name" value={formdata.name} id="email" className='form-control mb-2' placeholder='Enter  your Username'  required  onChange={Handler}/>
 
                         <label htmlFor="email">Email Address <span className='text-danger'>*</span></label>
-                        <input type="text" name="" id="email" className='form-control mb-2' placeholder='Enter  your Email'  required />
+                        <input type="text" name="email" value={formdata.email} id="email" className='form-control mb-2' placeholder='Enter  your Email'  required onChange={Handler} />
 
                         <label htmlFor="" className='form-label'>Password <span  className='text-danger'>*</span></label>
-                        <input type="text" className='form-control' placeholder='Enter your Password' />
+                        <input type="password" className='form-control'  name ="password" value={formdata.password} placeholder='Enter your Password' onChange={Handler} />
 
                         {/* <p className='small text-end mt-2'><a href="">Forgot Password</a></p>
                         <br /> */}
@@ -24,6 +51,7 @@ function Signup() {
                         <button className='btn btn-success' type='submit'> Create Account</button>
                     </div>
                 </div>
+              </form>
             </div>
         </div>
     </div>
