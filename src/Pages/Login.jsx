cfc571b44005487c1370baee4fc5a7ea62ae0da2
RoutuPrabhakara <routuprabhakar2000@gmail.com>
2025-02-13 22:41:32 +0530

    updaytes

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Login() {
+
+    
   return (
     <div>
         <div className="container-fluid p3 p-md-5 bg-light mt-4 mb-4">
@@ -10,10 +12,10 @@ function Login() {
                         <h1 className="fs-3 text-center fw-blod">Login</h1>
 
                         <label htmlFor="email">Email Address <span className='text-danger'>*</span></label>
-                        <input type="text" name="" id="email" className='form-control mb-2' placeholder='Enter  your Email'  required />
+                        <input type="text" name="name"  id="email" className='form-control mb-2' placeholder='Enter  your Email'  required />
 
                         <label htmlFor="" className='form-label'>Password <span  className='text-danger'>*</span></label>
-                        <input type="text" className='form-control' placeholder='Enter your Password' />
+                        <input type="text" className='form-control' name ="email"  placeholder='Enter your Password' />
 
                         <p className='small text-end mt-2'><a href="">Forgot Password</a></p>
                         <br />
--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -2,21 +2,21 @@ import axios from 'axios'
 import React, { useState } from 'react'
 
 function Signup() {
-  const[formdata,SetFormData]=useState({
+  const[formsdata,SetFormData]=useState({
     name:'',
     email:'',
     password:''
 })
 
 const Handler =(e)=>{
-  SetFormData({...formdata,[e.target.name]:e.target.value})
+  SetFormData({...formsdata,[e.target.name]:e.target.value})
 }
 
 const submitHandler = async(e)=>{
   e.preventDefault();
   try {
    
-    await axios.post('http://localhost:5000/SignUp',formdata)
+    await axios.post('http://localhost:5000/SignUp',formsdata)
     .then((response)=>{
       console.log('Data is connected',response.data)
       alert('Registration Succesful')
@@ -34,16 +34,16 @@ const submitHandler = async(e)=>{
               <form action="" onSubmit={submitHandler}>
               <div className="col-md-5 m-auto">
                     <div className="card p-3 shadow-sm">
-                        <h1 className="fs-3 text-center fw-blod">Create Account</h1>
+                        <h1 className="fs-3 text-center fw-bold">Create Account</h1>
 
                         <label htmlFor="email">Username <span className='text-danger'>*</span></label>
-                        <input type="text" name="name" value={formdata.name} id="email" className='form-control mb-2' placeholder='Enter  your Username'  required  onChange={Handler}/>
+                        <input type="text" name="name" value={formsdata.name}  id="email" className='form-control mb-2' placeholder='Enter  your Username'  required  onChange={Handler}/>
 
                         <label htmlFor="email">Email Address <span className='text-danger'>*</span></label>
-                        <input type="text" name="email" value={formdata.email} id="email" className='form-control mb-2' placeholder='Enter  your Email'  required onChange={Handler} />
+                        <input type="text" name="email" value={formsdata.email} id="email" className='form-control mb-2' placeholder='Enter  your Email'  required onChange={Handler} />
 
                         <label htmlFor="" className='form-label'>Password <span  className='text-danger'>*</span></label>
-                        <input type="password" className='form-control'  name ="password" value={formdata.password} placeholder='Enter your Password' onChange={Handler} />
+                        <input type="password" className='form-control'  name ="password" value={formsdata.password} placeholder='Enter your Password' onChange={Handler} />
 
                         {/* <p className='small text-end mt-2'><a href="">Forgot Password</a></p>
                         <br /> */}
